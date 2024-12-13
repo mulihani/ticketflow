beab2ff0b01e2b4a0c5600768fe88b02f8b58243
Majed Mulihani <majed@modernsys.net>
2024-12-14 02:41:02 +0300

    Export PDF file by the user
    Add the ability to export the ticket as a PDF file by the user.

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
 
 class PdfController extends Controller
 {
@@ -23,5 +24,25 @@ class PdfController extends Controller
 
         // Output the PDF 'I' for inline, 'D' for download
         return $mpdf->Output($view.'.pdf', 'D');
+    }
+
+
+    function generateUserTicketPdf($view = '', Ticket $record ) {
+
+        $mpdf = new \Mpdf\Mpdf();
+
+        if ($view != '' && $record != '') {
+            $html = view('reports.'.$view.'-report', ['record' => $record])->render();
+        
+            // Write HTML to the mPDF object
+            $mpdf->WriteHTML($html);
+
+            // Output the PDF 'I' for inline, 'D' for download
+            return $mpdf->Output($view.'.pdf', 'D');
+        
+        } else {
+            abort(404, 'Record not found');
+        }
+
     }
 }
